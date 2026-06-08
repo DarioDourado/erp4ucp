5ac84ca0ece557ab647df2ccb444f37f3e0d1b77
Dario Dourado <dariodourado@gmail.com>
2026-06-08 10:36:30 +0100

    seedDatabase

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             TaxRateSeeder::class,
+            SupermarketDataSeeder::class,
         ]);
 
         User::factory()->create([
